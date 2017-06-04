70fcbdca60284b8e37e9206243694e133f0d9c5d
Eugenio Barquin <eu.barquin@me.com>
2017-06-04 22:24:26 +0200

    Dish class created

---
+++ app/src/main/java/com/eugeniobarquin/restaurant/Dish.java
@@ -0,0 +1,49 @@
+package com.eugeniobarquin.restaurant;
+
+
+public class Dish {
+
+    private String mName;
+    private int mImage;
+    private String mAlergens;
+    private Float mPrice;
+
+    public Dish(String name, int image, String alergens, Float price) {
+        mName = name;
+        mImage = image;
+        mAlergens = alergens;
+        mPrice = price;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public int getImage() {
+        return mImage;
+    }
+
+    public void setImage(int image) {
+        mImage = image;
+    }
+
+    public String getAlergens() {
+        return mAlergens;
+    }
+
+    public void setAlergens(String alergens) {
+        mAlergens = alergens;
+    }
+
+    public Float getPrice() {
+        return mPrice;
+    }
+
+    public void setPrice(Float price) {
+        mPrice = price;
+    }
+}
