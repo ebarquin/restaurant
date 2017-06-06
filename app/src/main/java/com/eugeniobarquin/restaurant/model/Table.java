02e20d1c4dce9ae13212adf85e009b4727dee34e
Eugenio Barquin <eu.barquin@me.com>
2017-06-06 13:59:57 +0200

    Table class created

---
+++ app/src/main/java/com/eugeniobarquin/restaurant/model/Table.java
@@ -0,0 +1,28 @@
+package com.eugeniobarquin.restaurant.model;
+
+
+import com.eugeniobarquin.restaurant.R;
+
+import java.util.LinkedList;
+
+public class Table {
+    private LinkedList<Dish> mDishes;
+
+    //Fake Table
+    public Table() {
+        mDishes = new LinkedList<>();
+        mDishes.add(new Dish("Ensalada", R.drawable.ensalada, "Gluten", 12, "Poco hecho"));
+        mDishes.add(new Dish("Paella", R.drawable.paella, "Cacahuetes", 7, "Al punto"));
+        mDishes.add(new Dish("Pollo asado", R.drawable.pollo_asado, "Gluten", 6, "Muy hecho y sin patatas"));
+        mDishes.add(new Dish("Sopa", R.drawable.sopa, "Gluten", 15, "Muy caliente"));
+    }
+
+    public Dish getDish(int index) {
+        return mDishes.get(index);
+    }
+
+    public int getCount() {
+        return mDishes.size();
+    }
+
+}
